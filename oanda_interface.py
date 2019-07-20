50b3015519ca37f371e84d71b83c301296098578
choH <henryzhongwork@hotmail.com>
2019-07-19 23:35:03 -0400

    ✨ Wrap DataFrame  into class to preserve record props.

--- oanda_interface.py
+++ oanda_interface.py
@@ -13,6 +13,22 @@ import datetime
 import pandas as pd
 import numpy as np
 
+csv_export_dir = './test_data/'
+my_account_id = '101-001-11707432-001'
+my_access_token = 'a10a1538b3fdaf3a8a2f3b9a496a3aeb-00a7b8f54791fb3132b22ca77419cc58'
+
+class Onada_Record:
+    def __init__(self, _record, _record_path, _from, _to, _interval, _currency_pair):
+        self.record_df = _record
+        self.record_path = _record_path
+        self.currency_pair = _currency_pair
+        self.from_time = _from
+        self.to_time = _to
+        self.request_interval = _interval
+        self.rows = _record.shape[0]
+
+    def record_review(self):
+        print("\n\t{:20}{}\n\t{:20}{}\n\t{:20}{}\n\t{:20}{}\n\t{:20}{}\n\t{:20}{}\n".format('file path:', self.record_path, 'currency pair(s): ', self.currency_pair, 'from: ', self.from_time, 'to: ', self.to_time, 'interval: ', self.request_interval, 'total rows:', self.rows))
 
 
 class Oanda_Interface:
@@ -22,7 +38,7 @@ class Oanda_Interface:
         self.nationality = "USA"
         self.client = oandapyV20.API(access_token=self.access_token)
 
-    def get_history_price(self, _from, _to, _interval, _currency_pair):
+    def get_history_price(self, _from, _to, _interval, _currency_pair, close_price_only = False):
         _params = {
             "from": _from,
             "to": _to,
@@ -35,49 +51,35 @@ class Oanda_Interface:
         df = existed_df.rename(index=str, columns={"mid.o": "open_price", "mid.h": "high_price", "mid.l": "low_price", "mid.c": "close_price"})
         df = df.reindex(columns=['time', 'open_price', 'high_price', 'low_price', 'close_price', 'complete', 'volume'])
 
-        csv_filename = './dummy_data/' + _currency_pair + '_' + _from[0:19] + '_' + _to[0:19] + '_' + _interval + '.csv'
+        csv_filename = csv_export_dir + _currency_pair + '_' + _from[0:19] + '_' + _to[0:19] + '_' + _interval + '.csv'
+        if close_price_only:
+            df = df.drop(['open_price', 'high_price', 'low_price', 'complete'], axis =1)
+            df = df.rename(index=str, columns={'close_price': _currency_pair + '_close', 'volume': _currency_pair + '_volume'})
+            # print(df)
         df.to_csv(csv_filename, index = None, header = True)
-        print("\n\nThe requested record has been successfully exported.\n\t{:16}{}\n\t{:16}{}\n\t{:16}{}\n\t{:16}{}\n\t{:16}{}\n\t{:16}{}\n".format('file path:', csv_filename, 'currency pair: ', _currency_pair, 'from: ', _from, 'to: ', _to, 'interval: ', _interval, 'total rows:', df.shape[0]))
-        return df
+
+
+        temp_OR = Onada_Record(df, csv_filename, _from, _to, _interval, _currency_pair)
+        print("\n\nThe requested record has been successfully exported.")
+        temp_OR.record_review()
+        return temp_OR
 
     def get_history_price_segment(self, _currency_pair, _params):
         for r in InstrumentsCandlesFactory(instrument = _currency_pair, params = _params):
-            print("REQUEST: {} {} {}".format(r, r.__class__.__name__, r.params))
+            # print("REQUEST: {} {} {}".format(r, r.__class__.__name__, r.params))
             record_json = self.client.request(r)
             new_df = pd.io.json.json_normalize(record_json['candles'])
             yield new_df
 
 
-my_account_id = '101-001-11707432-001'
-my_access_token = 'a10a1538b3fdaf3a8a2f3b9a496a3aeb-00a7b8f54791fb3132b22ca77419cc58'
-OI = Oanda_Interface(my_account_id, my_access_token)
 
-# _from = "2016-01-01T00:00:00Z"
-# _to = "2016-12-31T23:59:59Z"
-# _interval = "H1"
-# _currency_pair = "USD_JPY"
+# OI = Oanda_Interface(my_account_id, my_access_token)
 
-# _from = "2017-01-01T00:00:00Z"
-# _to = "2017-12-31T23:59:59Z"
-# _interval = "H1"
-# _currency_pair = "USD_JPY"
-
-# _from = "2018-01-01T00:00:00Z"
-# _to = "2018-12-31T23:59:59Z"
-# _interval = "H1"
-# _currency_pair = "USD_JPY"
-
-
-# _from = "2018-01-01T00:00:00Z"
-# _to = "2018-06-30T23:59:59Z"
+# _from = "2019-01-01T00:00:00Z"
+# _to = "2019-01-02T00:00:00Z"
 # _interval = "M1"
-# _currency_pair = "USD_JPY"
+# USD_JPY_currency_pair = "USD_JPY"
+# GBP_JPY_currency_pair = "GBP_JPY"
+# GBP_USD_currency_pair = "GBP_USD"
 
-
-# _from = "2018-07-01T00:00:00Z"
-# _to = "2018-12-31T23:59:59Z"
-# _interval = "M1"
-# _currency_pair = "USD_JPY"
-
-# df = OI.get_history_price(_from, _to, _interval, _currency_pair)
-# df
+# OR_test = OI.get_history_price(_from, _to, _interval, USD_JPY_currency_pair)
