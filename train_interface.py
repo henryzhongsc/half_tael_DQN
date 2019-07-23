50dbf2f2fbdd6fb911dd83bf1e2c9ba0ad919383
choH <henryzhongwork@hotmail.com>
2019-07-23 18:01:17 -0400

    🚨 Test input on exception handling.

--- train_interface.py
+++ train_interface.py
@@ -5,11 +5,15 @@ import trade_interface as TI
 _account_name = 'dev_train_interface'
 _currency_balance = {'USD': 30000, 'JPY': 30000, 'GBP': 30000}
 _from = "2019-01-01T00:00:00Z"
+# _to = "2018-01-02T00:00:00Z"
 _to = "2019-01-02T00:00:00Z"
 _interval = "M1"
 
 ###############################################################################
+
+# time_0 = '2019-01-01T22:03:00.000000000Z'
 time_1 = '2019-01-01T22:30:00.000000000Z'
+# time_2 = '2019-01-01T22:29:00.000000000Z'
 time_2 = '2019-01-01T22:41:00.000000000Z'
 time_3 = '2019-01-01T22:55:00.000000000Z'
 time_4 = '2019-01-01T22:59:00.000000000Z'
@@ -21,6 +25,7 @@ time_7 = '2019-01-01T23:55:00.000000000Z'
 if __name__ == "__main__":
     TI_train = TI.Trade_Interface(_account_name, _currency_balance, _from, _to, _interval)
 
+    # TI_train.execute_trade(time_0, 'USD', 'JPY', 100)
     TI_train.execute_trade(time_1, 'USD', 'GBP', 10)
     TI_train.execute_trade(time_2, 'GBP', 'USD', 20)
     TI_train.execute_trade(time_3, 'GBP', 'JPY', 30)
