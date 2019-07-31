7e7d028eeccdf89240ecad8dfada3bf98625ed6e
choH <henryzhongwork@hotmail.com>
2019-07-31 19:07:36 -0400

    ✨ Mimic trial_DQN_v1.0 demo run with data generated by trading platform.

--- DQN_v2.0/train_interface.py
+++ DQN_v2.0/train_interface.py
@@ -1,3 +1,69 @@
 from RL_env import FX
 from RL_brain import DQN
 
+
+def run_maze():
+    step = 0
+    for episode in range(5):
+        # initial observation
+        observation = env.reset()
+
+        # print("###############################################")
+        print(type(observation))
+        # print("###############################################")
+
+        while True:
+            # fresh env
+            #env.render()
+
+            # RL choose action based on observation
+
+            action = RL.choose_action(observation)
+
+            # RL take action and get next observation and reward
+            observation_, reward, done = env.step(action)
+
+            RL.store_transition(observation, action, reward, observation_)
+
+            if (step > 200) and (step % 5 == 0):
+                RL.learn()
+
+            # swap observation
+            # print("################# observation_ ############################")
+            # print(type(observation_))
+            # print("################### observation_ ##########################")
+            observation = observation_
+
+            # break while loop when end of this episode
+            if done:
+                print('game over')
+                print(env.balance)
+                # print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+                # print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$                             $$$$$$$$$$$$$$$$$$$$$$$$$$$")
+                # print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$                             $$$$$$$$$$$$$$$$$$$$$$$$$$$")
+                # print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+                break
+            step += 1
+
+    # end of game
+
+
+
+    #env.destroy()
+
+
+if __name__ == "__main__":
+    # maze game
+    env = FX()
+    RL = DQN(env.n_actions, env.n_features,
+                      learning_rate=0.01,
+                      reward_decay=0.9,
+                      e_greedy=0.9,
+                      replace_target_iter=200,
+                      memory_size=2000,
+                      # output_graph=True
+                      )
+#     env.after(100, run_maze)
+    run_maze()
+#     env.mainloop()
+    RL.plot_cost()
