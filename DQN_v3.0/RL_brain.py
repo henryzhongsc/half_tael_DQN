3ef23cf8e6bebec8754116296b71142e718c3770
choH <henryzhongwork@hotmail.com>
2019-08-16 16:35:17 -0400

    🔨 Update to support dynamic input (any amount of currencies). 💄 Remove unnecessary debug print out code.

--- DQN_v3.0/RL_brain.py
+++ DQN_v3.0/RL_brain.py
@@ -1,12 +1,4 @@
-"""
-This part of code is the DQN brain, which is a brain of the agent.
-All decisions are made in here.
-Using Tensorflow to build the neural network.
-View more on my tutorial page: https://morvanzhou.github.io/tutorials/
-Using:
-Tensorflow: 1.0
-gym: 0.7.3
-"""
+# Refenced [MorvanZhou/Reinforcement-learning-with-tensorflow/contents/5_Deep_Q_Network/](https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow/tree/master/contents/5_Deep_Q_Network)
 
 import numpy as np
 import pandas as pd
@@ -15,12 +7,11 @@ import tensorflow as tf
 np.random.seed(1)
 tf.set_random_seed(1)
 
-
 # Deep Q Network off-policy
 class DQN:
     def __init__(
             self,
-            n_currencys,
+            n_currency_pairs,
             n_actions,
             n_features,
             learning_rate=0.01,
@@ -32,7 +23,7 @@ class DQN:
             e_greedy_increment=None,
             output_graph=False,
     ):
-        self.n_currencys = n_currencys
+        self.n_currency_pairs = n_currency_pairs
         self.n_actions = n_actions
         self.n_features = n_features
         self.lr = learning_rate
@@ -44,36 +35,20 @@ class DQN:
         self.epsilon_increment = e_greedy_increment
         self.epsilon = 0 if e_greedy_increment is not None else self.epsilon_max
 
-        # total learning step
+
         self.learn_step_counter = 0
-
-        # initialize zero memory [s, a, r, s_]
-        # self.memory = np.zeros((self.memory_size, n_features * 4 + 2))
-        self.memory = np.zeros((self.memory_size, self.n_features * self.n_currencys * 2 + 2))
-        # print("@"*30)
-        # print(len(self.memory))
-        # print("@"*30)
-        # consist of [target_net, evaluate_net]
-
+        self.memory = np.zeros((self.memory_size, self.n_features * self.n_currency_pairs * 2 + 2))
         self._build_net()
-
         t_params = tf.get_collection('target_net_params')
         e_params = tf.get_collection('eval_net_params')
         self.replace_target_op = [tf.assign(t, e) for t, e in zip(t_params, e_params)]
-
         self.sess = tf.Session()
 
         if output_graph:
-            # $ tensorboard --logdir=logs
-            # tf.train.SummaryWriter soon be deprecated, use following
-            tf.summary.FileWriter("logs/", self.sess.graph)
+            tf.summary.FileWriter("logs/tensorboard_logs", self.sess.graph)
 
         self.sess.run(tf.global_variables_initializer())
         self.cost_his = []
-
-    # we will define functions for tensorflow use # ######################################################
-    # these are private methods                   # ######################################################
-    ######################################################################################################
 
     def weight_variable(self, shape):
         initial = tf.truncated_normal(shape, stddev=0.1)
@@ -84,58 +59,83 @@ class DQN:
         return tf.Variable(initial)
 
     def conv2d(self,x, W):
-        # temp = tf.nn.conv2d(x, W, strides=[1, 2, 25, 1], padding='SAME')
-        # temp_filter = tf.Variable(tf.random_normal([2, 2, 1, 1]))
         temp = tf.nn.conv2d(x, filter=W, strides=[1, 1, 1, 1], padding='SAME')
-        print('CONV2d !#!'*5)
-        print("\n\n x: {} \n W: {} \n temp: {} ".format(x, W, temp))
-
-        print('CONV2d !#!'*5)
+        # print('CONV2D'*5)
+        # print("\n\n x: {} \n W: {} \n temp: {} ".format(x, W, temp))
         return temp
 
-    def max_pool_2x2(self,x):
-        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    def max_pool_pairs_x_timestamps(self, input, n_pairs, n_timestamps):
+        return tf.nn.max_pool(input, ksize=[1, 2, 2, 1], strides=[1, n_pairs, n_timestamps, 1], padding='SAME')
 
 
     def _build_net(self):
         # ------------------ build evaluate_net ------------------
-        # self.x = tf.placeholder(tf.float32, [None, 600])
-        self.x = tf.placeholder(tf.float32, [None, self.n_features * self.n_currencys], name = 's')
-        # self.s = tf.reshape(self.x, [-1,2,300,1])
-        self.s = tf.reshape(self.x, [-1, self.n_currencys, self.n_features, 1])
-        # W = tf.Variable(tf.zeros([self.n_features*2,7]))
-        W = tf.Variable(tf.zeros([self.n_features * self.n_currencys, self.n_actions]))
-        b = tf.Variable(tf.zeros([7]))
+        self.x = tf.placeholder(tf.float32, [None, self.n_features * self.n_currency_pairs], name = 's')
+        self.s = tf.reshape(self.x, [-1, self.n_currency_pairs, self.n_features, 1])
+
+
+        W = tf.Variable(tf.zeros([self.n_features * self.n_currency_pairs, self.n_actions]))
+        b = tf.Variable(tf.zeros([self.n_actions]))
 
         self.q_target = tf.placeholder(tf.float32, [None, self.n_actions], name='Q_target')  # for calculating loss
 
         #########################################################
         with tf.variable_scope('eval_net'):
+            # c_names(collections_names) are the collections to store variables
             c_names = ['eval_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
-
+            # first layer. collections is used later when assign to target net
             with tf.variable_scope('l1'):
                 W_conv1 = self.weight_variable([3, 3, 1, 32])
                 b_conv1 = self.bias_variable([32])
 
-
-                # self.s = [-1,3,300,1]
-                # strides = [1, 2, 25, 1]
-                # filter = [6, 6, 1, 32]
+                # DQN_v2.5 setting:
+                    # self.s = [-1,3,300,1]
+                    # strides = [1, 2, 25, 1]
+                    # filter = [6, 6, 1, 32]
                 h_conv1 = tf.nn.relu(self.conv2d(self.s, W_conv1) + b_conv1)
-                h_pool1 = self.max_pool_2x2(h_conv1)
+                # print('@3'*10 + " before h_conv1_relu shape " + '@3'*10)
+                # print(self.conv2d(self.s, W_conv1) + b_conv1)
 
-                W_fc1 = self.weight_variable([3 * 3200, 100])  # 这一行要改，用colab 打印 输入 来协助完成
-                b_fc1 = self.bias_variable([100])                # 这一行要改，用colab 打印 输入 来协助完成
+                # print('@3'*10 + " before h_conv1_relu shape " + '@3.1'*10)
+                # print(h_conv1)
 
-                h_pool1_flat = tf.reshape(h_pool1, [-1, 3 * 3200])
+
+                h_pool1 = self.max_pool_pairs_x_timestamps(h_conv1, 2, 2)
+                # print('@4'*10 + " h_pool1 shape after max_pool " + '@4'*10)
+                # print(h_pool1)
+
+
+                n_rows_temp = np.prod(h_pool1.shape.as_list()[1: ])# 9600 for 3 currencies.
+                print(tf.size(h_pool1), type(tf.size(h_pool1)))
+                # print(n_rows_temp, type(n_rows_temp))
+                # W_fc1 = self.weight_variable([3 * 3200, 100])
+                # W_fc1 = self.weight_variable([self.n_currency_pairs * 3200, 100])
+
+                W_fc1 = self.weight_variable([n_rows_temp, 100])
+                # print('@5'*10 + " W_fc1 shape " + '@5'*10)
+                # print(W_fc1)
+
+                b_fc1 = self.bias_variable([100])
+                # print('@6'*10 + " b_fc1 shape " + '@6'*10)
+                # print(b_fc1)
+
+
+                # h_pool1_flat = tf.reshape(h_pool1, [-1, 3 * 3200])
+                # h_pool1_flat = tf.reshape(h_pool1, [-1, self.n_currency_pairs * 3200])
+                h_pool1_flat = tf.reshape(h_pool1, [-1, n_rows_temp])
+                # print('@7'*10 + " h_pool1_flat shape " + '@7'*10)
+                # print(h_pool1_flat)
+
                 h_fc1 = tf.nn.relu(tf.matmul(h_pool1_flat, W_fc1) + b_fc1)
+                # print('@8'*10 + " h_fc1 shape " + '@8'*10)
+                # print(h_fc1)
 
 
                 # self.keep_prob = tf.placeholder(tf.float32)
                 # h_fc1_drop = tf.nn.dropout(h_fc1, self.keep_prob)
             with tf.variable_scope('l2'):
-                W_fc2 = self.weight_variable([100, self.n_actions]) # 可以把10 改成 7，也就是 action_space
-                b_fc2 = self.bias_variable([self.n_actions])         # 可以把10 改成 7，也就是 action_space
+                W_fc2 = self.weight_variable([100, self.n_actions])
+                b_fc2 = self.bias_variable([self.n_actions])
 
                 self.q_eval=tf.nn.softmax(tf.matmul(h_fc1, W_fc2) + b_fc2)
                 print("\n\n self.q_eval.shape: {}".format(self.q_eval.shape))
@@ -147,46 +147,29 @@ class DQN:
 
 
         # ------------------ build target_net ------------------
+        self.x_ = tf.placeholder(tf.float32, [None, self.n_features * self.n_currency_pairs], name = 's_' )
+        self.s_ = tf.reshape(self.x_, [-1, self.n_currency_pairs, self.n_features, 1])
 
-        # self.x_ = tf.placeholder(tf.float32, [None, 600],name = 's_' )
-        self.x_ = tf.placeholder(tf.float32, [None, self.n_features * self.n_currencys], name = 's_' )
-        # self.s_ = tf.reshape(self.x_, [-1,2,300,1])
-        self.s_ = tf.reshape(self.x_, [-1, self.n_currencys, self.n_features, 1])
-
-        #self.s_ = tf.placeholder(tf.float32, [-1,2,self.n_features,1],name = 's_' )    # input
         with tf.variable_scope('target_net'):
-            # c_names(collections_names) are the collections to store variables
             c_names = ['target_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
-            # first layer. collections is used later when assign to target net
             with tf.variable_scope('l1'):
 
                 W_conv1 = self.weight_variable([3, 3, 1, 32])
                 b_conv1 = self.bias_variable([32])
 
-
-                # self.s = [-1,3,300,1]
-                # strides = [1, 2, 25, 1]
-                # filter = [6, 6, 1, 32]
                 h_conv1 = tf.nn.relu(self.conv2d(self.s, W_conv1) + b_conv1)
-                h_pool1 = self.max_pool_2x2(h_conv1)
-
-                W_fc1 = self.weight_variable([3 * 3200, 100])  # 这一行要改，用colab 打印 输入 来协助完成
-                b_fc1 = self.bias_variable([100])                # 这一行要改，用colab 打印 输入 来协助完成
-
-                h_pool1_flat = tf.reshape(h_pool1, [-1, 3*3200])
+                h_pool1 = self.max_pool_pairs_x_timestamps(h_conv1, 2, 2)
+                n_rows_temp = np.prod(h_pool1.shape.as_list()[1: ])
+                W_fc1 = self.weight_variable([n_rows_temp, 100])
+                b_fc1 = self.bias_variable([100])
+                h_pool1_flat = tf.reshape(h_pool1, [-1, n_rows_temp])
                 h_fc1 = tf.nn.relu(tf.matmul(h_pool1_flat, W_fc1) + b_fc1)
 
-
-                # self.keep_prob = tf.placeholder(tf.float32)
-                # h_fc1_drop = tf.nn.dropout(h_fc1, self.keep_prob)
             with tf.variable_scope('l2'):
-                W_fc2 = self.weight_variable([100, self.n_actions]) # 可以把10 改成 7，也就是 action_space
-                b_fc2 = self.bias_variable([self.n_actions])         # 可以把10 改成 7，也就是 action_space
+                W_fc2 = self.weight_variable([100, self.n_actions])
+                b_fc2 = self.bias_variable([self.n_actions])
 
                 self.q_next=tf.nn.softmax(tf.matmul(h_fc1, W_fc2) + b_fc2)
-
-
-
 
     def store_transition(self, s, a, r, s_):
         if not hasattr(self, 'memory_counter'):
@@ -194,28 +177,15 @@ class DQN:
 
         transition = np.hstack((s, [a, r], s_))
 
-        # print('TRANS $'*20)
-        # print(transition[0:10])
-        # print(transition[-10:])
-        # print('TRANS $'*20)
-
         index = self.memory_counter % self.memory_size
         self.memory[index, :] = transition
         self.memory_counter += 1
 
     def choose_action(self, observation):
-        # to have batch dimension when feed into tf placeholder
-
         observation = observation[np.newaxis, :]
-
-        # print('observation\n')
-        # print(observation)
-        # print(observation.shape)
-
         if np.random.uniform() < self.epsilon:
             # forward feed the observation and get q value for every actions
             actions_value = self.sess.run(self.q_eval, feed_dict={self.x: observation})
-            # actions_value = self.sess.run(self.q_eval, feed_dict={self.s: observation})
             action = np.argmax(actions_value)
         else:
             action = np.random.randint(0, self.n_actions)
@@ -234,102 +204,38 @@ class DQN:
             sample_index = np.random.choice(self.memory_counter, size=self.batch_size)
         batch_memory = self.memory[sample_index, :]
 
-        # print('SELF.S &'*20)
-        # print(self.s)
-
-        # temp_s_ = batch_memory[:, -self.n_features*2:]
-        temp_s_ = batch_memory[: , -self.n_features * self.n_currencys :]
-        # temp_s_ = batch_memory[:, -self.n_features:]
-        # feed_s_ = np.array(temp_s_).reshape(-1,2,300,1)
-        feed_s_ = np.array(temp_s_).reshape(-1, self.n_currencys, self.n_features, 1)
-
-        # temp_s = batch_memory[:, :self.n_features*2]
-        temp_s = batch_memory[: , : self.n_features * self.n_currencys]
-        # temp_s = batch_memory[:, :self.n_features]
-        # feed_s = np.array(temp_s).reshape(-1,2,300,1)
-        feed_s = np.array(temp_s).reshape(-1, self.n_currencys, self.n_features, 1)
-
-
-        # q_next = self.sess.run(self.q_next, feed_dict={
-        #         self.s_: feed_s_,  # fixed params
-        #         self.s: feed_s,  # newest params
-        #     })
-        #
-        # # print('@'*30)
-        #
-        # q_eval = self.sess.run(self.q_eval, feed_dict={
-        #         self.s_: feed_s_,  # fixed params
-        #         self.s: feed_s,  # newest params
-        #     })
+        temp_s_ = batch_memory[: , -self.n_features * self.n_currency_pairs :]
+        feed_s_ = np.array(temp_s_).reshape(-1, self.n_currency_pairs, self.n_features, 1)
+        temp_s = batch_memory[: , : self.n_features * self.n_currency_pairs]
+        feed_s = np.array(temp_s).reshape(-1, self.n_currency_pairs, self.n_features, 1)
 
         q_next, q_eval = self.sess.run([self.q_next, self.q_eval], feed_dict={
                 self.s_: feed_s_,  # fixed params
                 self.s: feed_s,  # newest params
             })
 
-        # print('@'*30)
-
-
-
-        # print("self.s_: ",self.s_)
-        # print("self.s: ",self.s)
-        #
-        # print('SELF.S &'*20)
-
-        # change q_target w.r.t q_eval's action
         q_target = q_eval.copy()
-
         batch_index = np.arange(self.batch_size, dtype=np.int32)
-        eval_act_index = batch_memory[:, self.n_features * self.n_currencys].astype(int)
-        reward = batch_memory[: , self.n_features * self.n_currencys + 1]
-
-        # print("TEMP^FEED "*20)
-        #
-        # print("\n\n\n temp_s_: {}\n temp_s_.shape: {}".format(temp_s_, temp_s_.shape))
-        # print("\n\n\n feed_s_: {}\n feed_s_.shape: {}".format(feed_s_, feed_s_.shape))
-        # print("\n\n\n temp_s: {}\n temp_s.shape: {}".format(temp_s, temp_s.shape))
-        # print("\n\n\n feed_s: {}\n feed_s.shape: {}".format(feed_s, feed_s.shape))
-        #
-        # print("TEMP^FEED "*20)
-        #
-        # print("REWARD^ "*20)
-        # print("batch_index: {} ({})\neval_act_index: {} ({})\n\n\nq_target[batch_index, eval_act_index]: {}\nq_target[b, e].shape: {}\n\n\nq_target: {}\nq_target.shape: {}".format(batch_index, len(batch_index), eval_act_index, len(eval_act_index), q_target[batch_index, eval_act_index], q_target[batch_index, eval_act_index].shape, q_target, q_target.shape))
-
-    ##############################################################################################################
-        # print("\n\n batch_index: {} ({})".format(batch_index, len(batch_index)))
-        # print("\n\n eval_act_index: {} ({})".format(eval_act_index, len(eval_act_index)))
-        # print("\n\n batch_memory: {}\n batch_memory.shape: {}".format(batch_memory, batch_memory.shape))
-    ##############################################################################################################
-
-        # print("\n\n q_target: {}\n q_target.shape: {}".format(q_target, q_target.shape))
-
-        # print("\n\n\nreward: {}\nreward.shape: {}".format(reward, reward.shape))
-        # print("\n\n\nnp.max(q_next): {}\nnp.max(q_next).shape: {}".format(np.max(q_next, axis=1), (np.max(q_next, axis=1).shape)))
-        # print("\n\n q_target: {}\n q_target.shape: {}".format(q_target, q_target.shape))
-        # print("\n\n\nq_next: {}\nq_next.shape: {}".format(q_next, q_next.shape))
-
-        #ValueError: operands could not be broadcast together with shapes (32,) (3200,)
+        eval_act_index = batch_memory[:, self.n_features * self.n_currency_pairs].astype(int)
+        reward = batch_memory[: , self.n_features * self.n_currency_pairs + 1]
         q_target[batch_index, eval_act_index] = reward + self.gamma * np.max(q_next, axis=1)
 
-        # print(q_target[batch_index, eval_act_index])
+        # print('batch_memory.shape {}'.format(batch_memory.shape))
+        #ValueError: operands could not be broadcast together with shapes (32,) (3200,)
+        # q_target.shape: (32, 7)
+        # batch_memory.shape: (32, 3602) four currencies. (32, 1802) three currencies.
 
-        # print("REWARD^ "*20)
-
-        # train eval network
-        _, self.cost = self.sess.run([self._train_op, self.loss],
-                                     feed_dict={self.s: feed_s,
-                                                self.q_target: q_target})
+        _, self.cost = self.sess.run([self._train_op, self.loss], feed_dict={self.s: feed_s, self.q_target: q_target})
         self.cost_his.append(self.cost)
 
-        # increasing epsilon
         self.epsilon = self.epsilon + self.epsilon_increment if self.epsilon < self.epsilon_max else self.epsilon_max
         self.learn_step_counter += 1
 
     def plot_cost(self):
         import matplotlib.pyplot as plt
-        print("COST^!^ "*20)
+
+        print('COST LOG: ')
         print(self.cost_his)
-        print("COST^!^ "*20)
         plt.plot(np.arange(len(self.cost_his)), self.cost_his)
         plt.ylabel('Cost')
         plt.xlabel('training steps')
